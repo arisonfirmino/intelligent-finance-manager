ee53b05e24ee79ab9c77891c0cc32548bc4f2343
Arison Firmino <arisonfirmino.dev@gmail.com>
2025-01-26 18:04:00 -0300

    refactor: improve session handling and user fetching logic

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,16 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/app/lib/auth";
 import { getUserSession } from "@/app/helpers/fetchUserData";
 
 import Container from "@/app/components/container";
 
 const Home = async () => {
-  const user = getUserSession();
+  const session = await getServerSession(authOptions);
+
+  if (!session) redirect("/signin");
+
+  const user = await getUserSession();
 
   if (!user) return null;
 
--- app/helpers/fetchUserData.ts
+++ app/helpers/fetchUserData.ts
@@ -1,12 +1,11 @@
 import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
 import { authOptions } from "@/app/lib/auth";
 import { db } from "@/app/lib/prisma";
 
 export const getUserSession = async () => {
   const session = await getServerSession(authOptions);
 
-  if (!session) redirect("/signin");
+  if (!session) return;
 
   const user = await db.user.findUnique({
     where: {
